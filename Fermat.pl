f779f6c659e23112e65abb18f5449dd4c5a42461
Anthony John Ripa <you@example.com>
2020-10-15 23:55:57 -0400

    Add support for zero factors.

--- Fermat.pl
+++ Fermat.pl
@@ -1,22 +1,27 @@
 
 %	Author:		Anthony John Ripa
-%	Date:		2020.09.01
+%	Date:		2020.10.15
 %	Fermat:		A Rule System for Constraints
 
 
 :- ['Leibniz'] .
 
-l(X  , Y) :- var(X) , ground(Y) ,			  X <- Y   , ! .
-l(X+C, Y) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
-l(C+X, Y) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
-l(X*C, Y) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
-l(C*X, Y) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
-
-e(X, Y) :- l(X,Y) , ! .
-e(X, Y) :- l(Y,X) , ! .
-e(X, Y) :- =(X,Y) , ! .
-e(X, Y) :- write(X),write(' ≟ '),write(Y) , ! .
-
 :- redefine_system_predicate(=(_,_)) .
 
-=(X,Y) :- e(X,Y).
+=(X  , Y) :- var(X) , ground(Y) ,			  X <- Y   , ! .
+=(Y  , X) :- var(X) , ground(Y) ,			  X <- Y   , ! .
+=(X+C, Y) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
+=(Y, X+C) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
+=(C+X, Y) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
+=(Y, C+X) :- var(X) , ground(Y) , ground(C) , X <- Y-C , ! .
+=(_*X,M*X):- var(M) ,    var(X)             , X =  0       .
+=(Y*X,M*X):- var(M) ,    var(X)             , M =  Y   , ! .
+=(M*X,_*X):- var(M) ,    var(X)             , X =  0       .
+=(M*X,Y*X):- var(M) ,    var(X)             , M =  Y   , ! .
+=(X*C, Y) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
+=(Y, X*C) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
+=(C*X, Y) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
+=(Y, C*X) :- var(X) , ground(Y) , ground(C) , X <- Y/C , ! .
+
+=(X, Y) :- =(X,Y) , ! .
+=(X, Y) :- write(X),write(' ≟ '),write(Y) , ! .
