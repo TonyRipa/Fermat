344a78ba577759a46b78c2264baa7eb8ca46f5e3
Anthony John Ripa <you@example.com>
2021-05-15 23:56:18 -0400

    Use Postfix.

--- Fermat.pl
+++ Fermat.pl
@@ -1,15 +1,23 @@
 
 %	Author:		Anthony John Ripa
-%	Date:		2021.04.15
+%	Date:		2021.05.15
 %	Fermat:		A Rule System for Constraints
 
 
 :- ['Leibniz'] .
 
-eval(X@X=Y  , _ ,  X ) :- show(('ev',X@ X =Y)) , 0/0 <- Y                          , ! .
-eval(X@C*X=Y, _ , Ans) :- show(('ev',X@C*X=Y)) ,   R <- Y/C , eval(X@X=R, _ , Ans) , ! .
-eval(X@X*C=Y, _ , Ans) :- show(('ev',X@X*C=Y)) ,   R <- Y/C , eval(X@X=R, _ , Ans) , ! .
-eval(X@Z*C=Y, _ , Ans) :- show(('ev',X@Z*C=Y)) ,   R <- Y/C , eval(X@Z=R, _ , Ans) , ! .
-eval(X@C*Z=Y, _ , Ans) :- show(('ev',X@C*Z=Y)) ,   R <- Y/C , eval(X@Z=R, _ , Ans) , ! .
+simps(InputString,Output) :- atom_chars(InputString,Atoms) , map(atom_string,Atoms,Strings) , map(string_term,Strings,Terms) , simp(Terms,Output) .
 
-eval(A@X=Y,N,E):- show(('ev',A@X=Y)) , if(is0(Y),norder(exp(X),N,R),R=exp(Y)) , replace(exp(X),R,A,B), go(B,C) , replace(X,Y,C,D) , go(D,E) , succ(('ev',E)) , ! .
+simp(Input,Output) :- Stack=[] , sim(Input,Stack,Output) . 
+
+sim(I,S,O) :- I=[     ]                                                               ,       =(S,O) .	%	Halt
+sim(I,S,O) :- I=[I1  |IT] , not(op(I1))                                  , S2=[I1|S]  , sim(IT,S2,O) .	%	Push Num
+sim(I,S,O) :- I=[+   |IT]               , S = [N1,N2|ST] , N3 <- N1 + N2 , S2=[N3|ST] , sim(IT,S2,O) .	%	Add
+sim(I,S,O) :- I=[+,N3|IT] , not(op(N3)) , S = [   N2|[]] , N1 <- N3 - N2 , S2=[N1]    , sim(IT,S2,O) .	%	Sub
+sim(I,S,O) :- I=[*   |IT]               , S = [N1,N2|ST] , N3 <- N1 * N2 , S2=[N3|ST] , sim(IT,S2,O) .	%	Mul
+sim(I,S,O) :- I=[*,N3|IT] , not(op(N3)) , S = [   N2|[]] , N1 <- N3 / N2 , S2=[N1]    , sim(IT,S2,O) .	%	Div
+
+op(*).
+op(+).
+
+string_term(S,T) :- term_string(T,S) .
