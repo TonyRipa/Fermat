ab5f5b2f3af3b7cb1ce640f36356186c332d6bb6
Anthony John Ripa <you@example.com>
2023-02-15 22:47:57 -0500

    integer(X) returns X in inf..sup .

--- Fermat.pl
+++ Fermat.pl
@@ -1,13 +1,19 @@
 
 %	Author:		Anthony John Ripa
-%	Date:		2023.1.15
+%	Date:		2023.02.15
 %	Fermat:		A Rule System for Constraints
 
 :- use_module(library(clpfd)).
 
+:- redefine_system_predicate(integer(_)) .
+
+integer(X)        :- int(X)                                             , ! .
+
+int(V)            :- var(V) ,                             V in inf..sup , ! .
+int(I)            :-          integer(I)                                , ! .
+
 simp(V = I)       :- var(V) , integer(I)                , V =  I        , ! .
 simp(V + I1 = I2) :- var(V) , integer(I1) , integer(I2) , V is I2 - I1      .
-simp(V * 0  = 0)  :- var(V) ,                             V in inf..sup , ! .
-simp(_ * 0  = 0)                                                            .
+simp(X * 0  = 0)  :- int(X)                                             , ! .
 simp(V * 0  = I)  :- var(V) , integer(I)                , V =  I / 0    , ! .
 simp(V * I1 = I2) :- var(V) , integer(I1) , integer(I2) , V is I2 / I1      .
